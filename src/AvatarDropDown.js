8a4f41d7e39e71ed45905226e921f340cd05a874
tornike Despotashvili <tornike.despotashvili@gmail.com>
2022-11-02 15:06:57 +0400

    updt

--- src/AvatarDropDown.js
+++ src/AvatarDropDown.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import './thirdPage.css';
 import dropDownArr from './images/dropDownArrow.svg'
+import player1 from './images/player1.svg'
+import player2 from './images/player2.svg'
+import player3 from './images/player3.svg'
+import player4 from './images/player4.svg'
 
 function Avatar() {
     const [avatar, setAvatar] = useState('choose your Avatar')
     const [toggleArr, setToggleArr] = useState(false)
     const [menuArrow, setMenuArrow] = useState('')
+    const [showStyle, setShowStyleStyle] = useState('hiddenSkills')
 
     function toggleArrow() {
         if(!toggleArr){
             setMenuArrow('rotateArr')
+            setShowStyleStyle('showskill')
             setToggleArr(true)
         }else{
             setMenuArrow('rotateArrBack')
+            setShowStyleStyle('hiddenSkills')
             setToggleArr(false)
         }
    }
@@ -22,10 +29,28 @@ function Avatar() {
             <p>{avatar}</p>
             <img className={menuArrow} src={dropDownArr} />
 
-                <div>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                <div style={{marginTop:'100px'}}>
+                <div className={showStyle}>
+                    <div className="innderDIV">
+                        <p>Magnus Carlsen</p>
+                        <img src={player1} />
+                    </div>
+
+                    <div className="innderDIV">
+                        <p>Magnus Carlsen</p>
+                        <img src={player1} />
+                    </div>
+
+                    <div className="innderDIV">
+                        <p>Magnus Carlsen</p>
+                        <img src={player1} />
+                    </div>
+
+                    <div className="innderDIV">
+                        <p>Magnus Carlsen</p>
+                        <img src={player1} />
+                    </div>
+                </div>
                 </div>
         </div>
     )
